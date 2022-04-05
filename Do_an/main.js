386990af65eb3b7761ed8e2c643026c355c96b66
duongchikien1 <95597617+duongchikienk63@users.noreply.github.com>
2022-04-06 00:14:01 +0700

    do-an

--- Do_an/main.js
+++ Do_an/main.js
@@ -3,10 +3,10 @@ const dangNhap = document.getElementById("login");
 const loginPage = document.querySelector(".login");
 const loginClose = document.querySelector(".close");
 const loginSubmit = document.querySelector("login-submit");
-// const nextReg = document.getElementsByClassName("next_reg");
-// const nextLog = document.getElementsByClassName("next_log");
-// const loginEle = document.querySelector(".login-content");
-// const regEle = document.querySelector(".registration-content");
+const nextReg = document.getElementById("next-reg");
+const nextLog = document.getElementById("next-log");
+const loginEle = document.querySelector(".login-content");
+const regEle = document.querySelector(".registration-content");
 const fixedMenu = document.querySelector(".header-menu");
 const backTop = document.querySelector(".back_top");
 
@@ -18,18 +18,52 @@ loginClose.addEventListener("click", function () {
   loginPage.classList.add("display-none");
 });
 
-window.addEventListener('scroll', (e) => {
-  if(window.scrollY >= 35) {
+nextReg.addEventListener("click", function () {
+  loginEle.classList.add("display-none");
+  regEle.classList.remove("display-none");
+});
+
+nextLog.addEventListener("click", function () {
+  regEle.classList.add("display-none");
+  loginEle.classList.remove("display-none");
+});
+
+// Sự kiện cuộn màn hình
+window.addEventListener("scroll", (e) => {
+  if (window.scrollY >= 35) {
     fixedMenu.classList.add("fixed-menu");
   } else {
-    fixedMenu.classList.remove("fixed-menu")
+    fixedMenu.classList.remove("fixed-menu");
   }
-})
+});
 
-window.addEventListener('scroll', (e) => {
-  if(window.scrollY < 1200) {
+window.addEventListener("scroll", (e) => {
+  if (window.scrollY < 1200) {
     backTop.classList.add("display-none");
   } else {
     backTop.classList.remove("display-none");
   }
+});
+
+// Thêm, bớt sản phẩm
+let btnAdd = document.querySelector("#add");
+let btnMinus = document.querySelector("#minus");
+let totalProduct = document.getElementById("total-products");
+
+btnAdd.addEventListener("click", function () {
+  totalProduct.innerText = Number(totalProduct.innerText) + 1;
+});
+
+btnMinus.addEventListener("click", function () {
+  if ( Number(totalProduct.innerText) > 1) {
+    totalProduct.innerText = Number(totalProduct.innerText) - 1;
+  }
+});
+
+// Xóa sản phẩm trong giỏ hàng
+let delEle = document.getElementById("detele");
+let productsCartEle = document.querySelector(".products-cart");
+
+delEle.addEventListener("click", function() {
+  productsCartEle.classList.add("display-none");
 })
